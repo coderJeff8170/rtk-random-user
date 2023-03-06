72fdf954ba41d08b10ea378ab0a3e9548c3fe0b9
Jeff Andrews <babajeff8170@gmail.com>
2023-03-06 18:21:12 -0500

    app working and done!

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { usersApi } from "../services/users";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { getDefaultNormalizer } from "@testing-library/react";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(usersApi.middleware),
 });
+
+setupListeners(store.dispatch);
